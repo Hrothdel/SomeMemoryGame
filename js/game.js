eeee74031f90a8912369c5d846bf51599d8eb9c4
Leo Pop <leo32pop@gmail.com>
2017-10-28 22:56:44 +0300

    Implemented grid creation

--- js/game.js
+++ js/game.js
@@ -1,7 +1,30 @@
-let start_button = $('#start-button');
+const grid_container = $('#content');
 
-let startGame = function () {
+const default_width = 5,
+    default_height = 4;
 
+let cards = [];
+
+let start_button = $('#start-button'),
+    grid;
+
+function createGrid(height, width) {
+
+    for(let i = 0; i < height; i++) {
+        grid_container.append(`<tr class="grid-row"></tr>`);
+        let row = grid_container.children().last();
+
+        for(let j = 0; j < width; j++) {
+            let index = (i*width) + (j+1);
+            
+            cards.push(new Card(index));
+            cards[index-1].addTo(row);
+        }
+    }
+};
+
+function startGame() {
+    createGrid(default_height, default_width);
 };
 
 start_button.click(function () {
