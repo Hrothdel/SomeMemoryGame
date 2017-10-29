3dd58228f27a1491eef12143ce77fccec5025857
Leo Pop <leo32pop@gmail.com>
2017-10-29 15:34:09 +0200

    Added basic show function to cards

--- js/card-class.js
+++ js/card-class.js
@@ -3,5 +3,9 @@ let Card = function (number) {
 };
 
 Card.prototype.addTo = function(parent) {
-    parent.append(`<tc class="card">${this.number}</tc>`)
+    parent.append(`<tc class="card" id="${this.number}">${this.number}</tc>`)
+};
+
+Card.prototype.show = function() {
+    $(`#${this.number}`).css('background-color', 'black');
 };
