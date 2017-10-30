027d81cce69d05e4e0205bae635ad4d648f98fe4
Leo Pop <leo32pop@gmail.com>
2017-10-30 07:42:51 +0200

    Removed text from hidden cards

--- js/card-class.js
+++ js/card-class.js
@@ -1,24 +1,29 @@
 let Card = function (position) {
     this.position = position;
+    this.element = undefined;
 
     this.content = undefined;
     this.is_hidden = true;
 };
 
 Card.prototype.addTo = function (parent) {
-    parent.append(`<tc class="card" id="${this.position}">${this.content}</tc>`)
+    parent.append(`<tc class="card" id="${this.position}"></tc>`)
+    this.element = $(`#${this.position}`);
 };
 
 Card.prototype.show = function () {
-    $(`#${this.position}`).css('background-color', 'black');
-
     this.is_hidden = false;
+
+    this.element.css('background-color', 'black');
+    this.element.text(String(this.content));
+
 };
 
 Card.prototype.hide = function () {
+    this.is_hidden = true;
 
     setTimeout(function () {
-        $(`#${this.position}`).css('background-color', 'white');
+        this.element.css('background-color', 'white');
+        this.element.text('');
     }.bind(this), 1000);
-    this.is_hidden = true;
 };
